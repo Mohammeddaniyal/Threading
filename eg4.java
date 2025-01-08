741e32cd78c52ca39adad744c536efd612111706
daniyal <mohammeddaniyal453@gmail.com>
2025-01-08 19:31:58 +0530

    Added eg4.java : Why using invokeLater is important

---
+++ eg4.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class xyz extends JFrame
+{
+JTextArea ta;
+xyz()
+{
+Container container;
+ta=new JTextArea(10,100);
+ta.setLineWrap(true);
+ta.setWrapStyleWord(true);
+JScrollPane jsp=new JScrollPane(ta);
+JButton b=new JButton("Start");
+b.addActionListener(new ActionListener(){
+public void actionPerformed(ActionEvent ev)
+{
+aaa a=new aaa(xyz.this);
+ }
+});
+container=getContentPane();
+container.setLayout(new FlowLayout());
+container.add(jsp);
+container.add(b);
+setLocation(100,100);
+setVisible(true);
+setSize(900,900);
+
+}
+public void setNum(int num)
+{
+ta.append(String.valueOf(num));
+ta.append(" ");
+}
+}
+class aaa extends Thread
+{
+private xyz i;
+aaa(xyz i)
+{
+this.i=i;
+System.out.println("Before Invoking invokeLater");
+SwingUtilities.invokeLater(this);
+System.out.println("After Invoking invokeLater");
+}
+
+public void run()
+{
+System.out.println("About to set Number");
+for(int x=101;x<150;x++)
+{
+i.setNum(x);
+try
+{
+Thread.sleep(250);
+}catch(InterruptedException ie)
+{
+System.out.println(ie.getMessage());
+}
+}
+}
+}
+class psp
+{
+public static void main(String gg[])
+{
+xyz x=new xyz();
+}
+}
